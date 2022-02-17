c7a9cfa693f721d78a7125c184454fc3134b6cbb
Edison Zhuang <edisonzhuang@edisons-mbp.lan>
2022-02-18 01:13:34 +0800

    edited q1

--- assignment/q1-smart-home.js
+++ assignment/q1-smart-home.js
@@ -1,21 +1,18 @@
 /*
-    Create a function "createSignalFactory" that returns an object with function "send()". The implementation of "send()" function should
-    go according to the device type declared in the only argument.
-    You can use easily create this with object literals and functions. If you have succeeded this approach, you are being challenged to 
-    improve the code by:
-    - Create a "BaseSignal" class to be inherited by "TVSignal", "AirconSignal" and "DoorSignal" class.
+    Task
+    - Create a "BaseSignal" class to be inherited by "TvSignal", "AirconSignal" and "DoorSignal" class.
     - In the "BaseSignal" class, throw an error within the constructor to block direct instantiation (see doc-permissions.js for example).
-    - In the children classes, override "send()" function with implementation specfic to its type.
-    - Through the function "createSignalFactory", instantiate an object using the "new" keyword.
+    - Implement `send` method to print `Sending ${type} signal` in the BaseSignal class.
+    - In the respective child classes, call `super()` with it's type.
 */
 
-// Add code here
+// Task: Add code here
 
-let signal = createSignalFactory('tv');
-signal.send(); // "Send tv signal"
+const tv = new TvSignal();
+tv.send(); // prints "Sending tv signal"
 
-signal = createSignalFactory('aircon');
-signal.send(); // "Send aircon signal"
+const door = new DoorSignal();
+door.send(); // prints "Sending door signal"
 
-signal = createSignalFactory('door');
-signal.send(); // "Send door signal"
+const aircon = new AirconSignal();
+aircon.send(); // prints "Sending aircon signal"
