2e45339d28e26ffa2821d80437df979c594c5abd
Edison Z <edisen.zhuang@gmail.com>
2022-05-10 18:56:46 +0800

    simplified assignment question

---
+++ assignment/q1-class.js
@@ -0,0 +1,22 @@
+/*
+    Task:
+    - Instantiate an object by the name "car" using the new keyword.
+    - Call the drive() method of "car" instance.
+*/
+
+class Vehicle {
+
+    // Private properties (Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields)
+    #driverName = null;
+
+    // For object instantiation use (Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor)
+    constructor(driverName){
+        this.#driverName = driverName;
+    }
+
+    drive(){
+        console.log(`The vehicle is driven by ${this.#driverName}`);
+    }
+}
+
+// Add code here
