ad2ab052706af89965cb6bbde052cf798f30cc59
Edison Zhuang <edisen.zhuang@gmail.com>
2022-11-30 14:22:18 +0800

    added assignment folder

---
+++ assignment/q1-smart-home.js
@@ -0,0 +1,36 @@
+/*
+    Task
+    - Create a "BaseSignal" class to be inherited by "TvSignal", "AirconSignal" and "DoorSignal" class.
+    - In the "BaseSignal" class, throw an error within the constructor to block direct instantiation (see doc-permissions.js for example).
+    - Implement `send` method to print `Sending ${type} signal` in the BaseSignal class.
+    - In the respective child classes, call `super()` with it's type.
+*/
+
+// Task: Add code here
+
+class TvSignal extends BaseSignal {
+    constructor(){
+        // Add code here
+    }
+}
+
+class AirconSignal extends BaseSignal {
+    constructor(){
+        // Add code here
+    }
+}
+
+class DoorSignal extends BaseSignal {
+    constructor(){
+        // Add code here
+    }
+}
+
+const tv = new TvSignal();
+tv.send(); // prints "Sending tv signal"
+
+const door = new DoorSignal();
+door.send(); // prints "Sending door signal"
+
+const aircon = new AirconSignal();
+aircon.send(); // prints "Sending aircon signal"
