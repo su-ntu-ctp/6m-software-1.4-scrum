9f085ac63cf62ff6e04dc8aee8921683e82d56fd
Afifrifaie <afifrifaie@gmail.com>
2023-03-03 14:38:35 +0800

    added assignment

--- assignment/q1-smart-home.js
+++ assignment/q1-smart-home.js
@@ -7,22 +7,36 @@
 */
 
 // Task: Add code here
+class BaseSignal{
+    constructor(type){
+        if (this.constructor === BaseSignal){
+            throw new Error("This class cannot be instantiated");
+        }
+        this.type = type;
+    }
+    send(){
+        console.log(`Sending ${this.type} signal`);
+    }
+}
 
 class TvSignal extends BaseSignal {
     constructor(){
         // Add code here
+        super('tv');
     }
 }
 
 class AirconSignal extends BaseSignal {
     constructor(){
         // Add code here
+        super('aircon');
     }
 }
 
 class DoorSignal extends BaseSignal {
     constructor(){
         // Add code here
+        super('door');
     }
 }
 
