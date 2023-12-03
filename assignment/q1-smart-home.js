bddadea4fb57f1b7aa10ee1e06595daffb633b42
nhkhai@gmail.com <nhkhai@gmail.com>
2023-12-03 16:30:14 +0800

    Updated with my answers.

--- assignment/q1-smart-home.js
+++ assignment/q1-smart-home.js
@@ -5,32 +5,49 @@
     - Implement `send` method to print `Sending ${type} signal` in the BaseSignal class.
     - In the respective child classes, call `super()` with it's type.
 */
+// Create a "BaseSignal" class to be inherited by "TvSignal", "AirconSignal" and "DoorSignal" class.
+class BaseSignal {
+  constructor(type) {
+    // In the "BaseSignal" class, throw an error within the constructor to block direct instantiation (see doc-permissions.js for example).
+    if (this.constructor.name === "BaseSignal") {
+      throw new Error("This class cannot be instantiated");
+    }
 
-// Task: Add code here
+    this.type = type;
+  }
+
+  // Implement `send` method to print `Sending ${type} signal` in the BaseSignal class.
+  send() {
+    console.log(`Sending ${this.type} signal`);
+  }
+}
 
 class TvSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Call `super()` with the type.
+    super("tv");
+  }
 }
 
 class AirconSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Call `super()` with the type.
+    super("aircon");
+  }
 }
 
 class DoorSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Call `super()` with the type.
+    super("door");
+  }
 }
 
 const tv = new TvSignal();
-tv.send(); // prints "Sending tv signal"
+tv.send(); // Prints "Sending tv signal".
 
 const door = new DoorSignal();
-door.send(); // prints "Sending door signal"
+door.send(); // Prints "Sending door signal".
 
 const aircon = new AirconSignal();
-aircon.send(); // prints "Sending aircon signal"
+aircon.send(); // Prints "Sending aircon signal".
